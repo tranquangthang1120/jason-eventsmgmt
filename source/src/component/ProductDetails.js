1367c6c4bd1cddc5b6461900e550458c0ecc7da1
ISol2303 <tp.nam23@gmail.com>
2023-11-29 14:27:45 +0700

    Product.js

---
+++ source/src/component/ProductDetails.js
@@ -0,0 +1,16 @@
+function ProductDetails({product, addCart}){
+    return(
+        <div className='productDetails'>
+            <div><img src={product.image} width="100%" alt="image"/></div>
+            <div>
+            <img src= {product.image} />
+            <h4>{product.name}</h4>
+            <p>{product.description}</p>
+            <p>Giá: {product.price} $</p>
+                <button onClick={() => addCart(product)}>Add To Cart</button>
+            </div>
+        </div>
+    );
+}
+
+export default ProductDetails;
