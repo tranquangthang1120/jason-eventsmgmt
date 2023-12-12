9fabc619caea65792b206f4c3ec815ec501dc2bb
ISol2303 <tp.nam23@gmail.com>
2023-12-12 15:39:03 +0700

    new1-12/12/2023

--- source/src/component/CartItem.js
+++ source/src/component/CartItem.js
@@ -4,7 +4,7 @@ function CartItem({product, deleteCart}){
       <tr>
           <td><img src={product.image} alt="image" width="300px"/></td>
           <td><h5>{product.name}</h5></td>
-          <td><h6>{product.price} vnđ</h6></td>
+          <td><h6>${product.price}</h6></td>
           <td><button onClick={() => deleteCart(product.id)}>Xóa</button></td>
       </tr>
 
