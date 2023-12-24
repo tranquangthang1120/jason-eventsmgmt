bfabb0a13588309ef964bc53f1e85da062595419
ISol2303 <tp.nam23@gmail.com>
2023-12-24 21:48:11 +0700

    Update Singup.js

--- source/src/component/Singup.js
+++ source/src/component/Singup.js
@@ -1,55 +1,75 @@
-import { useState } from "react";
-import '../css/Login.css'
-import { useNavigate,Link } from "react-router-dom";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
 
+const SignUp = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
 
-function Login({checkLogin, errorLogin}){
-    const navigate = useNavigate();
-    const [username,setUsername] = useState('');
-    const [password,setPassword] = useState('');
-    const [Enterthepassword,setEnterthepassword] = useState('');
-    const handleLogin = (e) => {
-        e.preventDefault();
-        const checkUser = {username, password, Enterthepassword};
-        checkLogin(checkUser);
-        setUsername('');
-        setPassword('');
-        setEnterthepassword('');
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = {
+      username,
+      password,
+      email,
+    };
+
+    try {
+      const response = await axios.post("/api/users", data);
+      if (response.status === 201) {
+        setUsername("");
+        setPassword("");
+        setEmail("");
+
+        alert("Đăng ký thành công!");
+      } else {
+        alert(response.data.message);
+      }
+    } catch (error) {
+      alert(error.message);
     }
+  };
 
-    return(
-        <div class="Loges">
-        <div class="login">
-           
-        <form onSubmit={handleLogin}>
-            <h4>{errorLogin}</h4>
-            <table>
-                <tr>
-                    <td><b class="loginh3"><h3 >Sing Up</h3></b></td>
-                </tr>
-                 
-                <tr>
-                    <td>Username</td>
-                    <td><input type="text" class="password" value={username} 
-                        onChange={(e) => setUsername(e.target.value)}/></td>
-                </tr>
-                <tr>
-                    <td>Password</td>
-                    <td><input type="password" class="password" value={password} 
-                        onChange={(e) => setPassword(e.target.value)}/>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Enter The Password</td>
-                    <td><input type="password" class="password" value={Enterthepassword} 
-                        onChange={(e) => setEnterthepassword(e.target.value)}/></td>
-                </tr>
-                <tr> 
-                    <td><p>Already have an account? <Link to="/login">Login here</Link></p></td>
-                    <td><input type="submit" value="Sing Up" class="logup"/></td></tr>
-                </table>
-                </form>
-                </div>
-                </div>
-                );}
-export default Login;
+  useEffect(() => {
+    const loadUsers = async () => {
+      const response = await axios.get("/api/users");
+      if (response.status === 200) {
+        const users = response.data;
+        console.log(users);
+      }
+    };
+
+    loadUsers();
+  }, []);
+
+  return (
+    <div className="container">
+      <h1>Đăng ký</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Tên người dùng"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Mật khẩu"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button type="submit">Đăng ký</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
